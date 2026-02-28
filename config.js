aada5b8746d6e89768afe0956cb4c7770cfe83f7
Pulto <428ace7@gmail.com>
2026-02-28 02:56:11 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,17 @@
 const CONFIG = {
     version: "v1.0.1",
-    link: "https://23pulto.github.io/Pulto-s-Obfuscator/"
+    link: "https://23pulto.github.io/Pulto-s-Obfuscator/",
+    features: {
+        byteEncode: true,
+        fakeMath: true,
+        deadFlow: true,
+        wrapStrings: true,
+        variableRenaming: true,
+        controlFlowFlattening: true,
+        stringSplitting: true,
+        antiDebug: true,
+        integrityChecks: true,
+        asciiAntiSkid: false,
+        asciiStyle: 'clean' // 'clean' or 'vulgare'
+    }
 };
