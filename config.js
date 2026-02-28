d7e8265220a8e0d58da601caab07bd41968ee113
Pulto <428ace7@gmail.com>
2026-02-28 03:41:21 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,20 @@
 const CONFIG = {
     version: "v1.0.1",
-    discordUrl: "https://discord.gg/H3KWyw5mBy", 
-    musicUrl: "background_Music.mp3", 
-    verificationDelay: 3000, 
+    link: "https://23pulto.github.io/Pulto-s-Obfuscator/",
+    discordUrl: "https://discord.gg/YOUR_DISCORD_INVITE",
+    musicUrl: "background_Music.mp3",
+    verificationDelay: 3000,
     features: {
         byteEncode: true,
-        fakeMath: true,
-        deadFlow: true,
-        wrapStrings: true,
+        fakeMath: false,
+        deadFlow: false,
+        wrapStrings: false,
         variableRenaming: true,
-        antiDebug: true
+        stringSplitting: false,
+        controlFlowFlattening: false,
+        antiDebug: true,
+        integrityChecks: true,
+        asciiAntiSkid: false,
+        asciiStyle: 'clean'
     }
 };
